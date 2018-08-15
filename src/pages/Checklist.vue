567b2261275e5d38f13983b08e97d48dd695a97b
Frank Matranga <thefrankmatranga@gmail.com>
2018-08-14 22:12:07 -0400

    Fixed details element

--- src/pages/Checklist.vue
+++ src/pages/Checklist.vue
@@ -9,24 +9,25 @@
     
     hr.separator
     div(v-if="Object.keys(categories).length > 0")
-      details.category(open, v-for="(value, key) in categories")
-        summary.category-header.flex
+      .category(v-for="(value, key) in categories")
+        .category-header.flex
           h2.category-name {{ key }}
-            span.category-total {{ getCategoryTotal(key) }} total
           .new-item(v-show="editing")
             input(type="text", @keyup.enter="addItem(key, new_items[key], new_counts[key])", :placeholder="'New ' + key + ' item'", v-model="new_items[key]")
             input(type="number", @keyup.enter="addItem(key, new_items[key], new_counts[key])", value=1, min=1, max=100, v-model.number="new_counts[key]")
             button(type="button", @click="addItem(key, new_items[key], new_counts[key])") Add Item
             a.remove-item(href="#", @click="removeCategory(key)", :title="'Remove category ' + key")
               button X
-        .items
+
+        details.items(open)
+          summary {{ getCategoryTotal(key)  }} total
           ul(v-if="categories[key].length > 0")
             li.item(v-for="item, index in categories[key]")
               b.item-name {{ item.name }}
               
               div(v-if="editing")
                 input(type="number", min=0, v-model.number="categories[key][index].count", max=100)
-                a.remove-item(href="#", @click="removeItem(key, index)", :title="'Remove ' + key + ' item ' + item.name")
+                a.remove-item(href="#", @click.prevent="removeItem(key, index)", :title="'Remove ' + key + ' item ' + item.name")
                   button X
               .item-info(v-else, :class="{ 'is-done': item.progress == item.count }")
                 span.item-count {{ item.progress + ' / ' + item.count }}
