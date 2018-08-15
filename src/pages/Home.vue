2d6afbdf35489ae2eee32b6c00874a679dfc83ad
Frank Matranga <thefrankmatranga@gmail.com>
2018-08-14 20:32:11 -0400

    Basic homepage info

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,16 @@
 <template lang="pug">
   #home
-    h2 Home Page
+    h1 Home
 
-    router-link(to="/checklist/create") Create
+    section
+      h3 What is this?
+      p This is a simple site that allows you to create a custom college move in checklist, use it to track packing, and share your own list or view and user others'!
+      p #[img.rpi-logo(src="../assets/RPILogo.jpg")]
+      h3 What is it made with?
+      p This site is a VueJS Single Page Application (SPA) using no backend for storage, because sharing a list is entirely stored in the url.
+
+    router-link(to="/checklist/create")
+      button Create
 </template>
 
 <script>
@@ -12,6 +20,10 @@ export default {
 </script>
 
 <style scoped>
+.rpi-logo {
+  max-width: 200px;
+}
+
 h3 {
   margin: 40px 0 0;
 }
