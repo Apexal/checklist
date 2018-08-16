391cf6263e46aaa4c25a52117171ad36d7037503
Frank Matranga <thefrankmatranga@gmail.com>
2018-08-16 10:59:15 -0400

    Minimize

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 const config = require('./config.js');
 const fbApp = firebase.initializeApp(config);
