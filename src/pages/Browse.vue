05f5074c5b55e69416d5eabea542c3210d063868
Frank Matranga <thefrankmatranga@gmail.com>
2018-08-18 19:02:00 -0400

    Hidden browse page

---
+++ src/pages/Browse.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  #browse
+    h1 Browse Lists
+      small  {{ checklists.length }} total
+
+    .checklists
+      div(v-for="(list, key) in checklists")
+        h3 {{ getCategoryTitles(list).join(', ') }}
+        router-link(:to="{ path: `/checklist/${list['.key']}` }")
+          button Browse
+        hr
+</template>
+
+<script>
+import db from '../firebase.js';
+
+export default {
+  name: 'browse',
+  firebase: {
+    checklists: {
+      source: db.ref('/lists'),
+      readyCallback () {
+        // eslint-disable-next-line
+        console.log('Loaded lists from Firebase...');
+      }
+    }
+  },
+  methods: {
+    getCategoryTitles (list) {
+      return Object.keys(list).filter(key => key !== '.key');
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/router.js
+++ src/router.js
@@ -2,11 +2,12 @@ import VueRouter from 'vue-router';
 
 // Pages
 import Home from './pages/Home.vue';
-
 import Checklist from './pages/Checklist.vue';
+import Browse from './pages/Browse.vue';
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/checklist/browse', component: Browse },
   { path: '/checklist/:key', component: Checklist }
 ];
 
